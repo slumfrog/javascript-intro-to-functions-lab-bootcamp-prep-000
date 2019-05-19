95877e5121aecaa0bb688fa61c56f3d5bec21d04
Anil Kishan <kishan.anil@gmail.com>
2019-05-19 00:21:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,3 +26,14 @@ function sayHitoGrandma(string) {
  }
 }
 
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
+}
