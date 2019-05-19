a40f48144b1130a5b6943bd54fb1b77c1e7b740b
Anil Kishan <kishan.anil@gmail.com>
2019-05-19 00:15:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHitoGrandma(string) {
- if (string === string.toLowerCase()) {
-   return "I can't hear you!"
+ if (string === string.toLowerCase) {
+   console.log(return "I can't hear you!")
  }
- else if (string === string.toUpperCase()) {
+ else if (string === string.toUpperCase) {
    return "YES INDEED!"
  }
  else if (string === "I love you, Grandma") {
