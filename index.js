34ef11ed0d82156604fa04a8b0c2fff381866262
Anil Kishan <kishan.anil@gmail.com>
2019-05-19 00:23:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,15 +27,3 @@ function sayHiToGrandma(string){
       return 'I love you, too.';
     }
 }
-
-function sayHitoGrandma(string) {
- if (string.toLowerCase() === string) {
-   return "I can't hear you!"
- }
- else if (string === string.toUpperCase()) {
-   return "YES INDEED!"
- }
- else if (string === "I love you, Grandma.") {
-   return "I love you, too"
- }
-}
